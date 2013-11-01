cf55237c60b906492a14c4a7f046742e9768ae66
mlbrown08 <mlbrown7@uncg.edu>
2013-11-01 18:19:58 -0400

    Game Control loads pics randomly - still WIP

--- src/src/com/example/sortinggame/GameActivity.java
+++ src/src/com/example/sortinggame/GameActivity.java
@@ -2,7 +2,6 @@ package com.example.sortinggame;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
@@ -22,51 +21,67 @@ import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TableLayout;
 import android.widget.TableRow;
 
-public class GameActivity extends Activity implements OnTouchListener, OnDragListener {
+public class GameActivity extends Activity implements OnTouchListener,
+		OnDragListener {
 
 	SortingDB db;
 	private ImageView[] images;
-	//private ImageView[] sortedImages;
-	private ArrayList <Integer> imagePath;
-	ImageView img;
+	// private ImageView[] sortedImages;
+	private ArrayList<Integer> imagePath;
 	TableRow imagePool;
 	String level;
+	GameControl game;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		//force landscape
+
+		// force landscape
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-		    		
-		//Hide the action bar to increase play area
+
+		// Hide the action bar to increase play area
 		ActionBar actionBar = getActionBar();
 		actionBar.hide();
-		
-		//Intent intent = getIntent(); //Retrieve the intent
-		
-		//position is the integer value of the icons position in the grid
-		//int position = intent.getIntExtra(LevelActivity.Icon_Position, -1);
-		
+
+		// Intent intent = getIntent(); //Retrieve the intent
+
+		// position is the integer value of the icons position in the grid
+		// int position = intent.getIntExtra(LevelActivity.Icon_Position, -1);
+
 		setContentView(R.layout.activity_game);
-		
-		db = new SortingDB(this);
-		
+
 		Intent i = getIntent();
 		level = i.getExtras().getString(LevelActivity.LEVEL_NAME);
-        images = new ImageView[8]; 
-        //sortedImages = new ImageView[24];
-        imagePath = new ArrayList<Integer>();
+
+		game = new GameControl(this, level);
+		db = new SortingDB(this);
+
+		images = new ImageView[8];
+		// sortedImages = new ImageView[24];
+		imagePath = new ArrayList<Integer>();
 		loadCategoryBackground(level);
 		loadImages();
-		
-		//Allow drag and drop of images to categories
+
+		// Allow drag and drop of images to categories
 		(findViewById(R.id.category1)).setOnDragListener(this);
 		(findViewById(R.id.category2)).setOnDragListener(this);
 		(findViewById(R.id.category3)).setOnDragListener(this);
-			
+		
+		//loads category symbols
+		ImageView symbol1 = (ImageView)(findViewById(R.id.categoryImage1));
+		ImageView symbol2 = (ImageView)(findViewById(R.id.categoryImage2));
+		ImageView symbol3 = (ImageView)(findViewById(R.id.categoryImage3));
+		
+		if(game.getCategorySymbols(0).isPreloaded() == 1)
+			symbol1.setImageResource(getImageResource(game.getCategorySymbols(0).getPath()));
+		if(game.getCategorySymbols(1).isPreloaded() == 1)
+			symbol2.setImageResource(getImageResource(game.getCategorySymbols(1).getPath()));
+		if(game.getCategorySymbols(2).isPreloaded() == 1)
+			symbol3.setImageResource(getImageResource(game.getCategorySymbols(2).getPath()));
+
+
 	}
 
 	@Override
@@ -92,7 +107,7 @@ public class GameActivity extends Activity implements OnTouchListener, OnDragLis
 		int dragAction = dragEvent.getAction();
 		ImageView view = (ImageView) dragEvent.getLocalState();
 		ViewGroup from = (ViewGroup) view.getParent();
-		
+
 		if (dragEvent.getAction() == DragEvent.ACTION_DRAG_STARTED)
 			;
 		// do nothing
@@ -103,79 +118,85 @@ public class GameActivity extends Activity implements OnTouchListener, OnDragLis
 			;
 		// do nothing
 		else if (dragEvent.getAction() == DragEvent.ACTION_DROP) {
-			LinearLayout category = (LinearLayout)v;
+			LinearLayout category = (LinearLayout) v;
 			ImageView img = new ImageView(this);
-			img.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+			img.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
 			Drawable copyImg = view.getDrawable();
 			img.setImageDrawable(copyImg);
 			category.addView(img);
 			view.setImageResource(R.drawable.albertosaurus);
 			view.setVisibility(View.VISIBLE);
 			/*
-			if (checkForValidMove()){
-				from.removeView(view);
-				LinearLayout to = (LinearLayout) v;
-				to.addView(view);
+			 * if (checkForValidMove()){ from.removeView(view); LinearLayout to
+			 * = (LinearLayout) v; to.addView(view);
+			 * view.setVisibility(View.VISIBLE); } else{ }
+			 */
+		} else if (dragEvent.getAction() == DragEvent.ACTION_DRAG_ENDED) {
+			//snaps image back to item pool
+			if (!dragEvent.getResult())
 				view.setVisibility(View.VISIBLE);
-			}
-			else{
-			}
-				*/
 		}
-		else if (dragEvent.getAction() == DragEvent.ACTION_DRAG_ENDED);
-			//view.setVisibility(View.VISIBLE);
+		return true;
+	}
 
+	public boolean checkForValidMove() {
 		return true;
 	}
-	
-	public boolean checkForValidMove(){
-		return true;
-	}
-	
+
 	private void loadImages() {
-    	Cursor test = db.query("SELECT path FROM  Images", null);
-    	Class drawable = R.drawable.class;
-    	Class ids = R.id.class;
+		Class res = R.id.class;
 		Field field;
 		int identifier;
-		for(int i = 0; i < images.length; i++) {
+		for (int i = 0; i < images.length; i++) {
+			int x = i + 1;
 			try {
-				test.moveToNext();
-				int x = i + 1;
-				//field = ids.getField("categoryImage" + x);
-				//identifier = field.getInt(null);
-				//sortedImages[i] = (ImageView) findViewById(identifier);
-				//sortedImages[i].setOnDragListener(this);
-				if (i < images.length){
-					field = ids.getField("imagePool" + x);
-					identifier = field.getInt(null);
-					images[i] = (ImageView) findViewById(identifier);
-					field = drawable.getField(test.getString(test.getColumnIndex("path")));
-					identifier = field.getInt(null);
-					images[i].setImageResource(identifier);
-					images[i].setOnTouchListener(this);
-				}
+				field = res.getField("imagePool" + x);				
+				identifier = field.getInt(null);
+				images[i] = (ImageView)(findViewById(identifier));
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
-				Log.e("MyTag", "Failure to get drawable id. Path = " + test.getString(test.getColumnIndex("path")), e);
+				e.printStackTrace();
+			}
+			if(game.getImages(i).isPreloaded() == 1) {
+				identifier = getImageResource(game.getImages(i).getPath());
+				images[i].setImageResource(identifier);
+				images[i].setOnTouchListener(this);
 			}
 		}
-    }
+	}
+
 	private void loadCategoryBackground(String level) {
-    	Cursor test = db.query("SELECT background FROM Level WHERE name=?", new String[]{level});
-    	Class res = R.drawable.class;
+		Cursor test = db.query("SELECT background FROM Level WHERE name=?",
+				new String[] { level });
+		Class res = R.drawable.class;
 		Field field;
 		try {
 			test.moveToNext();
 			field = res.getField(test.getString(test.getColumnIndex("background")));
 			int identifier = field.getInt(null);
-			
+
 			LinearLayout layout = (LinearLayout) findViewById(R.id.categories);
 			layout.setBackgroundResource(identifier);
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			Log.e("MyTag", "Failure to get drawable id. Path = " + test.getString(test.getColumnIndex("path")), e);
+			Log.e("MyTag",
+					"Failure to get drawable id. Path = "
+							+ test.getString(test.getColumnIndex("path")), e);
+		}
+	}
+	
+	private int getImageResource(String path) {
+		Class res = R.drawable.class;
+		Field field;
+		int identifier;
+		try {
+			field = res.getField(path);
+			return identifier = field.getInt(null);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			Log.e("MyTag", "Failure to get drawable id. Path = " + path, e);
+			return 0;
 		}
 	}
 }
--- src/src/com/example/sortinggame/GameControl.java
+++ src/src/com/example/sortinggame/GameControl.java
@@ -1,5 +1,213 @@
 package com.example.sortinggame;
 
-public class GameControl {
+import android.content.Context;
+import android.database.Cursor;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GameControl {
+	private SortingDB db;
+	private Level level;
+	private Category[] categories;
+	private Image[] images;
+	private Image[] categorySymbols;
+	private int imagesSorted;
+	
+	
+	public GameControl(Context mContext,  String l)
+	{
+		db = new SortingDB(mContext);
+		level = new Level();
+		categories = new Category[3];
+		images = new Image[24];
+		categorySymbols = new Image[3];
+		imagesSorted = 0;
+		
+		loadLevel(l);
+		loadCategories();
+		loadImages();
+	}
+
+	private void loadLevel(String l) {
+		Cursor query = db.query("SELECT name, background FROM Level WHERE name=?", new String[]{l});;
+		
+		while(query.moveToNext()) {
+			level.setName(query.getString(query.getColumnIndex("name")));
+			level.setBackground(query.getString(query.getColumnIndex("background")));
+		}
+	}
+	
+	private void loadCategories() {
+		Cursor query = db.query("SELECT categoryName, position FROM Category WHERE levelName=?", new String[]{level.getName()});;
+		
+		for (int i = 0; i < categories.length; i++) {
+			query.moveToNext();
+			categories[i] = new Category();
+			categories[i].setName(query.getString(query.getColumnIndex("categoryName")));
+			categories[i].setPosition(query.getInt(query.getColumnIndex("position")));
+		}
+	}
+	
+	private void loadImages() {
+		Cursor query = db.query("SELECT path, Images.categoryName, preloaded, Category.position FROM Images, Category WHERE Images.categoryName = Category.categoryName", null);
+		int num1 = 0;
+		int num2 = 0;
+		int num3 = 0;
+		ArrayList<Integer> usedImages = new ArrayList<Integer>();
+		boolean addImage = true;
+		boolean imageLoaded = false;
+		Random rand = new Random();
+		
+		//randomized images
+		for (int i = 0; i < images.length; i++) {
+			imageLoaded = false;
+			while (imageLoaded == false) {
+				query.moveToFirst();
+				int row = rand.nextInt(query.getCount());
+				query.move(row);
+				
+				//checks to see if row has already been used
+				for(int x = 0; x < usedImages.size(); x++) {
+					if(row != usedImages.get(x))
+						addImage = true;
+					else
+						addImage = false;
+				}
+				
+				if(addImage == true) {
+					if(query.getInt(3) == 1 && num1 < 8) {			
+						images[i] = new Image();
+						images[i].setCatName(query.getString(query.getColumnIndex("categoryName")));
+						images[i].setPath(query.getString(query.getColumnIndex("path")));
+						images[i].setPreloaded(query.getInt(query.getColumnIndex("preloaded")));
+						num1++;
+						usedImages.add(row);
+						imageLoaded = true;
+					} else if(query.getInt(3) == 2 && num2 < 8) {
+						images[i] = new Image();
+						images[i].setCatName(query.getString(query.getColumnIndex("categoryName")));
+						images[i].setPath(query.getString(query.getColumnIndex("path")));
+						images[i].setPreloaded(query.getInt(query.getColumnIndex("preloaded")));
+						num2++;
+						usedImages.add(row);
+						imageLoaded = true;
+					} else if(query.getInt(3) == 3 && num3 < 8) {
+						images[i] = new Image();
+						images[i].setCatName(query.getString(query.getColumnIndex("categoryName")));
+						images[i].setPath(query.getString(query.getColumnIndex("path")));
+						images[i].setPreloaded(query.getInt(query.getColumnIndex("preloaded")));
+						num3++;
+						usedImages.add(row);
+						imageLoaded = true;
+					} else
+						imageLoaded = false;
+				}
+			}
+		}
+	
+		num1 = 0;
+		num2 = 0;
+		num3 = 0;
+		
+		//randomized category symbols
+		for (int i = 0; i < categorySymbols.length; i++) {
+			imageLoaded = false;
+			while (imageLoaded == false) {
+				query.moveToFirst();
+				int row = rand.nextInt(query.getCount());
+				query.move(row);
+				
+				//checks to see if row has already been used
+				for(int x = 0; x < usedImages.size(); x++) {
+					if(row != usedImages.get(x))
+						addImage = true;
+					else
+						addImage = false;
+				}
+				
+				if(addImage == true) {
+					if(query.getInt(3) == 1 && num1 < 1 && i == 0) {			
+						categorySymbols[i] = new Image();
+						categorySymbols[i].setCatName(query.getString(query.getColumnIndex("categoryName")));
+						categorySymbols[i].setPath(query.getString(query.getColumnIndex("path")));
+						categorySymbols[i].setPreloaded(query.getInt(query.getColumnIndex("preloaded")));
+						num1++;
+						usedImages.add(row);
+						imageLoaded = true;
+					} else if(query.getInt(3) == 2 && num2 < 1 && i == 1) {
+						categorySymbols[i] = new Image();
+						categorySymbols[i].setCatName(query.getString(query.getColumnIndex("categoryName")));
+						categorySymbols[i].setPath(query.getString(query.getColumnIndex("path")));
+						categorySymbols[i].setPreloaded(query.getInt(query.getColumnIndex("preloaded")));
+						num2++;
+						usedImages.add(row);
+						imageLoaded = true;
+					} else if(query.getInt(3) == 3 && num3 < 1 && i == 2) {
+						categorySymbols[i] = new Image();
+						categorySymbols[i].setCatName(query.getString(query.getColumnIndex("categoryName")));
+						categorySymbols[i].setPath(query.getString(query.getColumnIndex("path")));
+						categorySymbols[i].setPreloaded(query.getInt(query.getColumnIndex("preloaded")));
+						num3++;
+						usedImages.add(row);
+						imageLoaded = true;
+					} else
+						imageLoaded = false;
+				}
+			}
+		}
+	}
+	
+	public boolean checkMove(String viewTag) {
+		
+		return false;	
+	}
+
+	public Level getLevel() {
+		return level;
+	}
+
+
+	public void setLevel(Level level) {
+		this.level = level;
+	}
+
+
+	public Category[] getCategories() {
+		return categories;
+	}
+
+
+	public void setCategories(Category[] categories) {
+		this.categories = categories;
+	}
+
+	public Image getImages(int index) {
+		return images[index];
+	}
+
+	public void setImages(Image[] images) {
+		this.images = images;
+	}
+
+	public Image getCategorySymbols(int index) {
+		return categorySymbols[index];
+	}
+
+	public void setCategorySymbols(Image[] categorySymbols) {
+		this.categorySymbols = categorySymbols;
+	}
+
+	public int getImagesSorted() {
+		return imagesSorted;
+	}
+
+	public void setImagesSorted(int imagesSorted) {
+		this.imagesSorted = imagesSorted;
+	}
+	
+	public Image getNextImage() {
+		return images[imagesSorted + 8];
+	}
+	
 }
