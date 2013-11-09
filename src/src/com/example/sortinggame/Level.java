510bb3b5658dd034563bca55b095a9f25435f697
mlbrown08 <mlbrown7@uncg.edu>
2013-11-09 10:36:22 -0500

    updated entity classes to reflect new db

--- src/src/com/example/sortinggame/Level.java
+++ src/src/com/example/sortinggame/Level.java
@@ -3,21 +3,23 @@ package com.example.sortinggame;
 public class Level
 {
 	private String name,icon,background;
-	private boolean isPreLoaded;
+	private int isPreLoaded, completed;
 	
-	public Level(String lName, String iName, String bName, boolean pload)
+	public Level(String lName, String iName, String bName, int pload, int completed)
 	{
 		this.name = lName;
 		this.icon = iName;
 		this.background = bName;
 		this.isPreLoaded = pload;
+		this.completed = completed;
 	}
 	public Level()
 	{
-		this.name = null;
-		this.icon = null;
-		this.background = null;
-		this.isPreLoaded = false;
+		name = null;
+		icon = null;
+		background = null;
+		isPreLoaded = -1;
+		completed = -1;
 	}
 	public void setName(String lName)
 	{
@@ -41,11 +43,23 @@ public class Level
 	public void setBackground(String background) {
 		this.background = background;
 	}
-	public boolean isPreLoaded() {
+	public int isPreLoaded() {
 		return isPreLoaded;
 	}
-	public void setPreLoaded(boolean isPreLoaded) {
+	public void setPreLoaded(int isPreLoaded) {
 		this.isPreLoaded = isPreLoaded;
+	}
+	/**
+	 * @return the completed
+	 */
+	public int isCompleted() {
+		return completed;
+	}
+	/**
+	 * @param completed the completed to set
+	 */
+	public void setCompleted(int completed) {
+		this.completed = completed;
 	}
 	
 }
