dd5dd0e44d5a93405f320c3e5236e7d16cd0cefb
rtpampli <rtpampli@uncg.edu>
2013-11-09 14:24:23 -0500

    Gallery Access from Icon/Bgrnd buttons (WIP)
    Need to work on multiple select

--- src/src/com/example/sortinggame/EditLevelActivity.java
+++ src/src/com/example/sortinggame/EditLevelActivity.java
@@ -1,20 +1,21 @@
 package com.example.sortinggame;
 
-import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.database.Cursor;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.view.View;
 
 public class EditLevelActivity extends Activity {
 	
+	private static final int RESULT_LOAD_IMAGE = 1;
 	String level_name ="";
 	String cat1 ="";
 	String cat2 ="";
@@ -66,12 +67,31 @@ public class EditLevelActivity extends Activity {
 	}
 	
 	public void chooseFromGallery(View view){
-		Intent intent = new Intent();
-		// View  the gallery
+		Intent intent = new Intent(
+		Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+		startActivityForResult(intent, RESULT_LOAD_IMAGE);
+		/*// View  the gallery
 		intent.setAction(Intent.ACTION_VIEW);
 		intent.setData(android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
 		// Start the activity
-		startActivity(intent);
+		startActivity(intent);*/
+	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data){
+			Uri selectedImage = data.getData();
+			String[] filePathColumn = {MediaStore.Images.Media.DATA };
+			Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+			cursor.moveToFirst();
+			
+			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+			String picturePath = cursor.getString(columnIndex);
+			cursor.close();
+			System.out.println(picturePath);
+			
+		}
 	}
 
 }
